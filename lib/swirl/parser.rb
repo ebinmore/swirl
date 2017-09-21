5a06152dfb754b763d87517578f6c47350abd538
Eric Binmore <eric.binmore@voxmedia.com>
2017-09-21 15:05:09 -0400

    General clean up
    Most tests pass now, mocked CashWords needs updating.

--- lib/swirl/parser.rb
+++ lib/swirl/parser.rb
@@ -20,25 +20,23 @@ module Swirl
       # 1) let's ensure that there's a CashWord object, otherwise raise an exception
       raise NoAffiliateDatabaseError unless @cash_words
 
-
+      puts "original_html = #{@html}"
       # 2) go through all the tokens in @html and see if they exist in @cash_words
-      monetized_html = @html.split(/\s/).each do |token|
-        output = ""
-        if @cash_words.has_word?(token)
+      monetized_html = ""
+      @html.split(/\s/).each do |token|
+        puts "token = #{token}"
+        if @cash_words.has_word?(token.downcase)
           # a) the token is a word in our affiliate database, so let's make some $$$!
-            # 3) get the corresponding URI from @cash_words
-            uri = @cash_words.get_links_for_word(token)
-            link = "<a href=""#{uri.first}"">#{token}</a>"
-            # 4) append the URI and token to the output as a link
-            output << link << " "
+          links = @cash_words.get_links_for_word(token.downcase)
+          monetized_html << "<a href=\"#{links.first[:url]}\">#{token}</a>" << " "
         else
           # b) no $$$ :-(
-            # 5) append the token to the revised output
-          output << token << " "
+          monetized_html << token << " "
         end
-        output
+        puts "output = #{monetized_html}"
       end
 
+      puts "monetized_html = #{monetized_html}"
       # 6) return the monetized html and make $$$!
       monetized_html
     end
