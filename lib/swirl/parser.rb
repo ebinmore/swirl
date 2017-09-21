c238a708ebf97d55f523527fff04c48ab65a6bc4
Eric Binmore <eric.binmore@voxmedia.com>
2017-09-21 15:43:42 -0400

    Quiets output unless DEBUG

--- lib/swirl/parser.rb
+++ lib/swirl/parser.rb
@@ -20,24 +20,24 @@ module Swirl
       # 1) return the original html if no affiliate database is defined
       return @html unless @cash_words
 
-      puts "original_html = #{@html}"
+      puts "original_html = #{@html}" if ENV.include?("DEBUG")
       # 2) go through all the tokens in @html and see if they exist in @cash_words
       monetized_html = ""
       @html.split(/(\s|\W)/).each do |token|
-        puts "token = #{token}"
+        puts "token = #{token}" if ENV.include?("DEBUG")
         if @cash_words.has_word?(token.downcase)
           # a) the token is a word in our affiliate database, so let's make some $$$!
           links = @cash_words.get_links_for_word(token.downcase)
-          puts "links = #{links}"
+          puts "links = #{links}" if ENV.include?("DEBUG")
           monetized_html << "<a href=\"#{links.sort_by { |k| k["value"] }.last}\">#{token}</a>"
         else
           # b) no $$$ :-(
           monetized_html << token
         end
-        puts "output = #{monetized_html}"
+        puts "output = #{monetized_html}" if ENV.include?("DEBUG")
       end
 
-      puts "monetized_html = #{monetized_html}"
+      puts "monetized_html = #{monetized_html}" if ENV.include?("DEBUG")
       # 3) return the monetized html and make $$$!
       monetized_html
     end
