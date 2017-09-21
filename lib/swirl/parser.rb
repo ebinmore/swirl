2d6e9607fa13c75681b8d761c9c079037158e0b9
Eric Binmore <eric.binmore@voxmedia.com>
2017-09-21 14:52:35 -0400

    First pass at #money_making_html

--- lib/swirl/parser.rb
+++ lib/swirl/parser.rb
@@ -1,4 +1,6 @@
 module Swirl
+  class NoAffiliateDatabaseError < StandardError; end
+
   class Parser
 
     def initialize
@@ -15,7 +17,30 @@ module Swirl
     end
 
     def money_making_html
-      # do some magic here
+      # 1) let's ensure that there's a CashWord object, otherwise raise an exception
+      raise NoAffiliateDatabaseError unless @cash_words
+
+
+      # 2) go through all the tokens in @html and see if they exist in @cash_words
+      monetized_html = @html.split(/\s/).each do |token|
+        output = ""
+        if @cash_words.has_word?(token)
+          # a) the token is a word in our affiliate database, so let's make some $$$!
+            # 3) get the corresponding URI from @cash_words
+            uri = @cash_words.get_links_for_word(token)
+            link = "<a href=""#{uri.first}"">#{token}</a>"
+            # 4) append the URI and token to the output as a link
+            output << link << " "
+        else
+          # b) no $$$ :-(
+            # 5) append the token to the revised output
+          output << token << " "
+        end
+        output
+      end
+
+      # 6) return the monetized html and make $$$!
+      monetized_html
     end
 
   end
